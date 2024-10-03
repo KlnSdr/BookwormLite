99b680e8cf6ccfaae3cc5ff298254914b891e02e
KlnSdr <monsieurk1209@gmail.com>
2024-10-03 18:27:11 +0200

    smal fix in build script

--- src/bookworm/resource/static/src/buildFE.sh
+++ src/bookworm/resource/static/src/buildFE.sh
@@ -8,11 +8,11 @@ echo "building /students ======================"
 cd students || exit
 ed pack
 
-perl -i -pe 'next if /src="https/; s/src="/src="{{CONTEXT}}\/students/g' index.html
-perl -i -pe 'next if /src="https/; s/src="/src="{{CONTEXT}}\/students/g' docs//index.html
+perl -i -pe 'next if /src="https/; s/src="/src="{{CONTEXT}}\/students\//g' index.html
+perl -i -pe 'next if /src="https/; s/src="/src="{{CONTEXT}}\/students\//g' docs//index.html
 
-perl -i -pe 'next if /href="https/; s/href="/href="{{CONTEXT}}\/students/g' index.html
-perl -i -pe 'next if /href="https/; s/href="/href="{{CONTEXT}}\/students/g' docs/index.html
+perl -i -pe 'next if /href="https/; s/href="/href="{{CONTEXT}}\/students\//g' index.html
+perl -i -pe 'next if /href="https/; s/href="/href="{{CONTEXT}}\/students\//g' docs/index.html
 
 mkdir ../../out/students
 cp -r docs/* ../../out/students
