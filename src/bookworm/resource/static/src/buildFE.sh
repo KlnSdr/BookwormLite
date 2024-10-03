841aa667c8de5b9a264fd146695f7c8da4410a10
KlnSdr <monsieurk1209@gmail.com>
2024-10-03 18:18:49 +0200

    add build script for frontend

---
+++ src/bookworm/resource/static/src/buildFE.sh
@@ -0,0 +1,19 @@
+cd .. || exit
+rm -rf out
+mkdir out
+
+cd src || exit
+
+echo "building /students ======================"
+cd students || exit
+ed pack
+
+perl -i -pe 'next if /src="https/; s/src="/src="{{CONTEXT}}\/students/g' index.html
+perl -i -pe 'next if /src="https/; s/src="/src="{{CONTEXT}}\/students/g' docs//index.html
+
+perl -i -pe 'next if /href="https/; s/href="/href="{{CONTEXT}}\/students/g' index.html
+perl -i -pe 'next if /href="https/; s/href="/href="{{CONTEXT}}\/students/g' docs/index.html
+
+mkdir ../../out/students
+cp -r docs/* ../../out/students
+rm -rf docs
