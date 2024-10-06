92e26fdc300fd8612b522474521173f2f5e86f96
KlnSdr <monsieurk1209@gmail.com>
2024-10-06 21:50:13 +0200

    add empty edom project for /books

--- src/bookworm/resource/static/src/buildFE.sh
+++ src/bookworm/resource/static/src/buildFE.sh
@@ -9,7 +9,7 @@ cd home || exit
 ed pack
 
 perl -i -pe 'next if /src="https/; s/src="/src="{{CONTEXT}}\//g' index.html
-perl -i -pe 'next if /src="https/; s/src="/src="{{CONTEXT}}\//g' docs//index.html
+perl -i -pe 'next if /src="https/; s/src="/src="{{CONTEXT}}\//g' docs/index.html
 
 perl -i -pe 'next if /href="https/; s/href="/href="{{CONTEXT}}\//g' index.html
 perl -i -pe 'next if /href="https/; s/href="/href="{{CONTEXT}}\//g' docs/index.html
@@ -22,11 +22,25 @@ cd ../students || exit
 ed pack
 
 perl -i -pe 'next if /src="https/; s/src="/src="{{CONTEXT}}\/students\//g' index.html
-perl -i -pe 'next if /src="https/; s/src="/src="{{CONTEXT}}\/students\//g' docs//index.html
+perl -i -pe 'next if /src="https/; s/src="/src="{{CONTEXT}}\/students\//g' docs/index.html
 
 perl -i -pe 'next if /href="https/; s/href="/href="{{CONTEXT}}\/students\//g' index.html
 perl -i -pe 'next if /href="https/; s/href="/href="{{CONTEXT}}\/students\//g' docs/index.html
 
 mkdir ../../out/students
 cp -r docs/* ../../out/students
+rm -rf docs
+
+echo "building /books ======================"
+cd ../books || exit
+ed pack
+
+perl -i -pe 'next if /src="https/; s/src="/src="{{CONTEXT}}\/books\//g' index.html
+perl -i -pe 'next if /src="https/; s/src="/src="{{CONTEXT}}\/books\//g' docs/index.html
+
+perl -i -pe 'next if /href="https/; s/href="/href="{{CONTEXT}}\/books\//g' index.html
+perl -i -pe 'next if /href="https/; s/href="/href="{{CONTEXT}}\/books\//g' docs/index.html
+
+mkdir ../../out/books
+cp -r docs/* ../../out/books
 rm -rf docs
