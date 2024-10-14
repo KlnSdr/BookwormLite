0159bb5a7768c78538ef8d136f2339153070eb07
KlnSdr <monsieurk1209@gmail.com>
2024-10-14 22:24:00 +0200

    when clicking isGem checkbox the grades get updated & the other cols are resetted

--- src/bookworm/resource/static/src/students/components/StudentsExplorer/StudentsExplorer.ts
+++ src/bookworm/resource/static/src/students/components/StudentsExplorer/StudentsExplorer.ts
@@ -1,4 +1,7 @@
 class StudentsExplorer implements Component {
+  private static readonly GRADES_GYM: number[] = [5, 6, 7, 8, 9, 10, 11, 12];
+  private static readonly GRADES_GS: number[] = [5, 6, 7, 8, 9, 10];
+
   public render(parent: edomElement) {
     edom.fromTemplate([this.instructions()], parent);
   }
@@ -6,62 +9,88 @@ class StudentsExplorer implements Component {
   public instructions(): edomTemplate {
     return {
       tag: "div",
+      children: [
+        {
+          ...new Checkbox("Gemeinschaftsschule:", () => {}).instructions(),
+          handler: [
+            {
+              id: "onChange",
+              type: "click",
+              body: (self: edomElement) => this.toggleGsGym(self),
+            },
+          ],
+        },
+        this.getBaseGymInstructions(),
+      ],
+    };
+  }
+
+  private getBaseGSInstructions(): edomTemplate {
+    return {
+      tag: "div",
       classes: ["studentsExplorer"],
       children: [
         {
           tag: "div",
           classes: ["studentsExplorerColumn"],
-          children: [
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-          ],
+          children: StudentsExplorer.GRADES_GS.map((grade: number) =>
+            new Button(grade.toString(), () => {}).instructions(),
+          ),
         },
         {
           tag: "div",
           classes: ["studentsExplorerColumn"],
-          children: [
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-          ],
+          children: [],
         },
         {
           tag: "div",
           classes: ["studentsExplorerColumn"],
-          children: [
-            new Button("x", (self: edomElement) => {}).instructions(),
-            new Button("x", (self: edomElement) => {}).instructions(),
-          ],
+          children: [],
         },
       ],
     };
+  }
+
+  private getBaseGymInstructions(): edomTemplate {
+    return {
+      tag: "div",
+      classes: ["studentsExplorer"],
+      children: [
+        {
+          tag: "div",
+          classes: ["studentsExplorerColumn"],
+          children: StudentsExplorer.GRADES_GYM.map((grade: number) =>
+            new Button(grade.toString(), () => {}).instructions(),
+          ),
+        },
+        {
+          tag: "div",
+          classes: ["studentsExplorerColumn"],
+          children: [],
+        },
+        {
+          tag: "div",
+          classes: ["studentsExplorerColumn"],
+          children: [],
+        },
+      ],
+    };
+  }
+
+  private toggleGsGym(self: edomElement) {
+    const isGem: boolean = (self.children[1].element as HTMLInputElement)
+      .checked;
+    this.clearColumns(self.parent!);
+
+    if (isGem) {
+      edom.fromTemplate([this.getBaseGSInstructions()], self.parent!);
+    } else {
+      edom.fromTemplate([this.getBaseGymInstructions()], self.parent!);
+    }
+  }
+
+  private clearColumns(container: edomElement) {
+    container.children[1].delete();
   }
 
   public unload() {}
