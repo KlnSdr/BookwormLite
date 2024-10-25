3e9d5e1404b113257fa52e898e13c42be6d5ba68
KlnSdr <monsieurk1209@gmail.com>
2024-10-25 09:39:58 +0200

    add endpoint to get usage of all books for a grade

--- src/bookworm/books/service/StudentBookAssociationService.java
+++ src/bookworm/books/service/StudentBookAssociationService.java
@@ -4,11 +4,14 @@ import bookworm.books.Book;
 import bookworm.books.StudentBook;
 import bookworm.books.StudentBookAssociation;
 import bookworm.students.Student;
+import bookworm.students.service.StudentsService;
 import dobby.util.json.NewJson;
 import janus.Janus;
 import thot.connector.Connector;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 public class StudentBookAssociationService {
@@ -36,6 +39,12 @@ public class StudentBookAssociationService {
         return getStudentBooks(bookAssocJson);
     }
 
+    public StudentBook[] getUsageOfBook(UUID bookId, int grade) {
+        final NewJson[] bookAssocJson = Connector.readPattern(BUCKET_NAME, ".*_" + bookId.toString(), NewJson.class);
+
+        return getStudentBooks(bookAssocJson, true, grade);
+    }
+
     public StudentBook[] getBooksForStudent(UUID studentId) {
         final NewJson[] bookAssocJson = Connector.readPattern(BUCKET_NAME, studentId.toString() + "_.*", NewJson.class);
 
@@ -43,6 +52,10 @@ public class StudentBookAssociationService {
     }
 
     private StudentBook[] getStudentBooks(NewJson[] bookAssocJson) {
+        return getStudentBooks(bookAssocJson, false, -1);
+    }
+
+    private StudentBook[] getStudentBooks(NewJson[] bookAssocJson, boolean doFilter, int grade) {
         if (bookAssocJson == null) {
             return null;
         }
@@ -52,12 +65,33 @@ public class StudentBookAssociationService {
             bookAssoc[i] = Janus.parse(bookAssocJson[i], StudentBookAssociation.class);
         }
 
-        final UUID[] bookIds = Arrays.stream(bookAssoc).map(StudentBookAssociation::getBookId).toArray(UUID[]::new);
+        final StudentBookAssociation[] bookAssocFiltered;
+        if (doFilter) {
+            final List<StudentBookAssociation> arrBookAssocFiltered = new ArrayList<>();
+            for (StudentBookAssociation assoc : bookAssoc) {
+                final UUID studentId = assoc.getStudentId();
+                final Student student = StudentsService.getInstance().find(studentId);
+
+                if (student == null) {
+                    continue;
+                }
+
+                if (student.getGrade() == grade) {
+                    arrBookAssocFiltered.add(assoc);
+                }
+            }
+
+            bookAssocFiltered = arrBookAssocFiltered.toArray(new StudentBookAssociation[0]);
+        } else {
+            bookAssocFiltered = bookAssoc;
+        }
+
+        final UUID[] bookIds = Arrays.stream(bookAssocFiltered).map(StudentBookAssociation::getBookId).toArray(UUID[]::new);
 
         final StudentBook[] result = new StudentBook[bookIds.length];
         for (int i = 0; i < bookIds.length; i++) {
             final Book book = BooksService.getInstance().find(bookIds[i]);
-            result[i] = new StudentBook(book, bookAssoc[i].getType());
+            result[i] = new StudentBook(book, bookAssocFiltered[i].getType());
         }
 
         return result;
--- src/bookworm/evaluation/rest/EvaluationResource.java
+++ src/bookworm/evaluation/rest/EvaluationResource.java
@@ -1,20 +1,24 @@
 package bookworm.evaluation.rest;
 
+import bookworm.books.Book;
 import bookworm.books.StudentBook;
+import bookworm.books.service.BooksService;
 import bookworm.books.service.StudentBookAssociationService;
 import bookworm.students.Student;
 import bookworm.students.service.StudentsService;
 import dobby.annotations.Get;
 import dobby.io.HttpContext;
 import dobby.io.response.ResponseCodes;
+import dobby.util.Tupel;
 import dobby.util.json.NewJson;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class EvaluationResource {
     private static final String BASE_PATH = "/rest/evaluation";
     private static final StudentsService studentService = StudentsService.getInstance();
     private static final StudentBookAssociationService assocService = StudentBookAssociationService.getInstance();
+    private static final BooksService booksService = BooksService.getInstance();
 
     @Get(BASE_PATH + "/{type}/grade/{grade}/financial-data")
     public void getFinancialDataForGrade(HttpContext context) {
@@ -134,5 +138,76 @@ public class EvaluationResource {
         final NewJson payload = new NewJson();
         payload.setList("students", responseList.stream().map(e -> (Object) e).toList());
         context.getResponse().setBody(payload);
+    }
+
+    @Get(BASE_PATH + "/{type}/grade/{grade}/books")
+    public void getBookEvaluation(HttpContext context) {
+        final String schoolType = context.getRequest().getParam("type");
+        final boolean isGem = schoolType.equalsIgnoreCase("gem");
+
+        if (!(schoolType.equalsIgnoreCase("gem") || schoolType.equalsIgnoreCase("gym"))) {
+            final NewJson payload = new NewJson();
+            payload.setString("msg", "Invalid school form.");
+
+            context.getResponse().setCode(ResponseCodes.BAD_REQUEST);
+            context.getResponse().setBody(payload);
+            return;
+        }
+
+        final String stringGrade = context.getRequest().getParam("grade");
+        final int grade;
+        try {
+            grade = Integer.parseInt(stringGrade);
+        } catch (Exception e) {
+            final NewJson payload = new NewJson();
+            payload.setString("msg", "Could not transform provided grade to int.");
+
+            context.getResponse().setCode(ResponseCodes.BAD_REQUEST);
+            context.getResponse().setBody(payload);
+            return;
+        }
+
+        final List<Book> allBooks = booksService.getForGrade(grade, isGem);
+        final List<NewJson> bookInfo = new ArrayList<>();
+
+        for (Book book : allBooks) {
+            final StudentBook[] studentBooks = assocService.getUsageOfBook(book.getId(), grade);
+
+            final NewJson bookJson = getBookInfo(book, studentBooks);
+
+            bookInfo.add(bookJson);
+        }
+
+        final NewJson payload = new NewJson();
+        payload.setList("books", bookInfo.stream().map(e -> (Object) e).toList());
+        context.getResponse().setBody(payload);
+    }
+
+    private static NewJson getBookInfo(Book book, StudentBook[] filteredStudentBooks) {
+        double sumPriceBuy = 0.0;
+        double sumPriceBorrow = 0.0;
+        int buyCount = 0;
+        int borrowCount = 0;
+
+        for (StudentBook studentBook: filteredStudentBooks) {
+            switch(studentBook.getUsageType()) {
+                case BUY -> {
+                    sumPriceBuy += studentBook.getBook().getPrice();
+                    buyCount++;
+                }
+                case BORROW -> {
+                    sumPriceBorrow += studentBook.getBook().isApplyFee() ? studentBook.getBook().getPrice() : 0.0;
+                    borrowCount++;
+                }
+            }
+        }
+
+        final NewJson bookJson = new NewJson();
+        bookJson.setString("name", book.getName());
+        bookJson.setFloat("sumPriceBuy", sumPriceBuy);
+        bookJson.setFloat("sumPriceBorrow", sumPriceBorrow);
+        bookJson.setInt("buyCount", buyCount);
+        bookJson.setInt("borrowCount", borrowCount);
+        return bookJson;
     }
 }
