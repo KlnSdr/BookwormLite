1988bd226d312951a1ec4609424ad6b8678df135
KlnSdr <monsieurk1209@gmail.com>
2024-11-05 12:22:16 +0100

    style login page

--- src/bookworm/resource/static/src/login/components/App/App.ts
+++ src/bookworm/resource/static/src/login/components/App/App.ts
@@ -5,26 +5,11 @@ class App implements Component {
 
   public instructions(): edomTemplate {
     return {
-      tag: "div",
-      children: [
-        // @ts-ignore include from students project
-        new LabeledInput(
-          "Loginname:",
-          () => {},
-          "",
-          "inputUsername"
-        ).instructions(),
-        // @ts-ignore include from students project
-        new LabeledInput(
-          "Passwort:",
-          () => {},
-          "",
-          "inputPassword",
-          "password"
-        ).instructions(),
-        // @ts-ignore include from students project
-        new Button("anmelden", () => doLogin()).instructions(),
-      ],
+        tag: "div",
+        classes: ["app"],
+        children: [
+            new LoginPanel().instructions()
+        ]
     };
   }
 
---
+++ src/bookworm/resource/static/src/login/components/LoginPanel/LoginPanel.ts
@@ -0,0 +1,33 @@
+class LoginPanel implements Component {
+  public render(parent: edomElement) {
+    edom.fromTemplate([this.instructions()], parent);
+  }
+
+  public instructions(): edomTemplate {
+    return {
+      tag: "div",
+      classes: ["loginPanel"],
+      children: [
+        // @ts-ignore include from students project
+        new LabeledInput(
+          "Loginname:",
+          () => {},
+          "",
+          "inputUsername"
+        ).instructions(),
+        // @ts-ignore include from students project
+        new LabeledInput(
+          "Passwort:",
+          () => {},
+          "",
+          "inputPassword",
+          "password"
+        ).instructions(),
+        // @ts-ignore include from students project
+        new Button("anmelden", () => doLogin(), ["secondaryButton"]).instructions(),
+      ],
+    };
+  }
+
+  public unload() {}
+}
