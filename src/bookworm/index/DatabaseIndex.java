fd0d5e2c8311f0553a60a2edf5d7514b44c9172e
KlnSdr <monsieurk1209@gmail.com>
2024-11-09 13:15:50 +0100

    add class to be used as a db 'index'

---
+++ src/bookworm/index/DatabaseIndex.java
@@ -0,0 +1,60 @@
+package bookworm.index;
+
+import dobby.util.json.NewJson;
+import janus.DataClass;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public abstract class DatabaseIndex<K, T> implements DataClass {
+    protected Map<K, Set<String>> keys;
+
+    public DatabaseIndex() {
+        keys = new HashMap<>();
+    }
+
+    public abstract void index(T object);
+    public abstract void unIndex(T object);
+
+    public void setFor(K key, Set<String> values) {
+        keys.put(key, values);
+    }
+
+    public void add(K key, String value) {
+        if (!keys.containsKey(key)) {
+            keys.put(key, new HashSet<>());
+        }
+        keys.get(key).add(value);
+    }
+
+    public Set<String> getFor(K key) {
+        if (!keys.containsKey(key)) {
+            return new HashSet<>();
+        }
+        return keys.get(key);
+    }
+
+    public Map<K, Set<String>> getAll() {
+        return keys;
+    }
+
+    public void drop(K key) {
+        keys.remove(key);
+    }
+
+    public void dropAll() {
+        keys.clear();
+    }
+
+    public void remove(K key, String value) {
+        if (!keys.containsKey(key)) {
+            return;
+        }
+        keys.get(key).remove(value);
+    }
+
+    public abstract NewJson toJson();
+    public abstract void fromJson(NewJson json);
+}
---
+++ src/bookworm/index/StringDatabaseIndex.java
@@ -0,0 +1,56 @@
+package bookworm.index;
+
+import dobby.util.json.NewJson;
+import dobby.util.logging.Logger;
+
+import java.util.HashSet;
+import java.util.List;
+
+public abstract class StringDatabaseIndex<T> extends DatabaseIndex<String, T> {
+    private static final Logger LOGGER = new Logger(StringDatabaseIndex.class);
+
+    @Override
+    public NewJson toJson() {
+        final NewJson json = new NewJson();
+
+        for (String key : keys.keySet()) {
+            final List<String> values = keys.get(key).stream().toList();
+            json.setList(key, values.stream().map(s -> (Object)s).toList());
+        }
+
+        final List<String> keysList = keys.keySet().stream().toList();
+        json.setList("index_keys", keysList.stream().map(s -> (Object)s).toList());
+
+        return json;
+    }
+
+    @Override
+    public void fromJson(NewJson json) {
+        keys.clear();
+
+        if (!validateJson(json)) {
+            LOGGER.error("Invalid JSON for index");
+            return;
+        }
+
+        final List<String> keysList = json.getList("index_keys").stream().map(Object::toString).toList();
+        for (String key : keysList) {
+            final List<String> values = json.getList(key).stream().map(Object::toString).toList();
+            keys.put(key, new HashSet<>(values));
+        }
+    }
+
+    private boolean validateJson(NewJson json) {
+        if (!json.hasKey("index_keys")) {
+            return false;
+        }
+
+        final List<String> keysList = json.getList("index_keys").stream().map(Object::toString).toList();
+        for (String key : keysList) {
+            if (!json.hasKey(key)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
